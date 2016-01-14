d77351b1267c8b5340ca3f6ac68364cdc8160068
singh <singh@singh-PC>
2016-01-14 15:50:35 +0530

    this is the java multiple statement program

---
+++ javamultiple/src/javamultiple/Javamultiple.java
@@ -0,0 +1,30 @@
+/*
+*java multiple statement
+* This program compute square root
+*/
+package javamultiple;
+
+/**
+ *author@ animesh singh
+ * 
+ */
+public class Javamultiple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //now all started
+        double x=5; //declaration and initialization
+        /* If you want a square root of any other number just change 
+         * the value of x.
+        */
+        
+        double y;
+        y=Math.sqrt(x);
+        System.out.println("y ="+ y);
+        //all ends here
+        
+    }
+    
+}
